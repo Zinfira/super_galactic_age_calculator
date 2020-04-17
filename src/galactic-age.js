a3c99a7269b059a0445a91e03713a07fbf6cbd82
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-17 11:45:35 -0700

    Add business logic to determine age on Venus and pass test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -2,9 +2,14 @@ export class ageCount {
   constructor(age) {
     this.age = age;
     this.mercuryAge = 0;
+    this.venusAge = 0;
   }
 
   determineAgeOnMercury() {
     this.mercuryAge = parseFloat((this.age / 0.24).toFixed(2));
+  }
+
+  determineVenusAge() {
+    this.venusAge = parseFloat((this.age / 0.62).toFixed(2));
   }
 }
