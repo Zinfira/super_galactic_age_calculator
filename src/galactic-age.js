e970f80e6bb20620941ef65236c394bd674d3388
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-17 11:22:13 -0700

    Add determine age on Mercury logic  and fail test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,10 +1,10 @@
 export class ageCount {
   constructor(age) {
     this.age = age;
-    this.mercuryAge = mercuryAge;
+    this.mercuryAge = 0;
   }
 
   determineAgeOnMercury() {
-    
+    this.mercuryAge += (this.age / 0.24);
   }
 }
